4e1076cd4f58f69960b0dad47b284410019c0fd6
Matteo Gavagnin <m@macteo.it>
2016-10-18 10:10:29 +0200

    Initial Commit

---
+++ Awesome/Awesome.h
@@ -0,0 +1,19 @@
+//
+//  Awesome.h
+//  Awesome
+//
+//  Created by Matteo Gavagnin on 18/10/2016.
+//  Copyright © 2016 Dolomate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Awesome.
+FOUNDATION_EXPORT double AwesomeVersionNumber;
+
+//! Project version string for Awesome.
+FOUNDATION_EXPORT const unsigned char AwesomeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Awesome/PublicHeader.h>
+
+
