f2456b09c57a75de85d9aeaeb37b7e0f8e0edd30
Matteo Gavagnin <m@macteo.it>
2016-10-18 10:34:05 +0200

    Added license

---
+++ Awesome/AwesomeLogger.h
@@ -0,0 +1,13 @@
+//
+//  AwesomeLogger.h
+//  Awesome
+//
+//  Created by Matteo Gavagnin on 18/10/2016.
+//  Copyright © 2016 Dolomate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AwesomeLogger : NSObject
+
+@end
---
+++ Awesome/AwesomeLogger.m
@@ -0,0 +1,13 @@
+//
+//  AwesomeLogger.m
+//  Awesome
+//
+//  Created by Matteo Gavagnin on 18/10/2016.
+//  Copyright © 2016 Dolomate. All rights reserved.
+//
+
+#import "AwesomeLogger.h"
+
+@implementation AwesomeLogger
+
+@end
